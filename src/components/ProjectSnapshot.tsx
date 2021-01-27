65241a62bd2f7ad089bcf6e0b5db5db98ec00225
Henry Shen <Henry.Shen@pgtrust.co.nz>
2021-01-27 19:25:21 +1300

    WIP2. Added more to the project snapshot

--- src/components/ProjectSnapshot.tsx
+++ src/components/ProjectSnapshot.tsx
@@ -1,17 +1,36 @@
 import React from 'react'
 
 class ProjectSnapshot extends React.Component<any, any> {
+  state = {
+    showDescription: false
+  }
+
+  toggleDescription() {
+    this.setState({showDescription: !this.state.showDescription})
+  }
 
   render() {
     return (
       <div className="snapshot-container">
         <h3>{this.props.project.name}</h3>
         <div className="bottom-project-wrapper">
-          <img alt="thumbnail" src={require(`../assets/thumbnails/${this.props.project.thumbnail}`)} width="200" height="200"/>
-          <span>{this.props.project.descriptions[0]}</span>
+          <img 
+            alt="thumbnail" 
+            src={require(`../assets/thumbnails/${this.props.project.thumbnail}`)} 
+            className="project-picture"
+            onClick={() => this.toggleDescription()}
+          />
+          <div className="project-text">
+            {
+              this.state.showDescription ? 
+              this.props.project.descriptions.map((description: string) => (
+                <span>{description}</span>
+              )) :
+              <span>{this.props.project.brief}</span>
+            }
+          </div>
+          
         </div>
-        
-        
       </div>
     )
   }
