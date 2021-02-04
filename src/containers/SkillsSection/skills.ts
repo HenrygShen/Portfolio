a711976272718275d993e7156fd3fb6617bf9a8c
Henry Shen <Henry.Shen@pgtrust.co.nz>
2021-02-04 15:34:04 +1300

    Fix bug after merge

--- src/containers/SkillsSection/skills.ts
+++ src/containers/SkillsSection/skills.ts
@@ -1,4 +1,4 @@
-const skills = [
+const skills: Array<[string,number]> = [
 	["C#/.NET", 90], ["Java", 85], ["Selenium", 85], ["HTML", 85], ["CSS", 80], 
 	["JavaScript", 80], ["React", 75], ["Python", 70], ["C++", 65], ["SQL", 65]]
 
