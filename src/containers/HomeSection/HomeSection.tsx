ae6f676b520b9884bc1ef5d7013601455a156917
Henry Shen <Henry.Shen@pgtrust.co.nz>
2021-02-04 20:27:18 +1300

    Added logos and links for socials. Also redid the icon

--- src/containers/HomeSection/HomeSection.tsx
+++ src/containers/HomeSection/HomeSection.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Button from '../../components/Button';
+import EmailLogo from '../../components/svgs/EmailLogo';
+import GithubLogo from '../../components/svgs/GithubLogo';
+import LinkedinLogo from '../../components/svgs/LinkedinLogo';
 
 class HomeSection extends React.Component<any, any> {
 
@@ -9,6 +12,24 @@ class HomeSection extends React.Component<any, any> {
         <span>I'm Henry.</span>
         <span>Situated in New Zealand.</span>
         <span>Software developer &amp; casual baker.</span>
+        <div className="logo-links">
+          <div className="logo">
+            <a href="https://www.linkedin.com/in/henry-shen-868a4b174/">
+              <LinkedinLogo />
+            </a>
+          </div>
+          <div className="logo">
+            <a href="https://github.com/HenrygShen">
+              <GithubLogo />
+            </a>
+          </div>
+          <div className="logo">
+            <a href="mailto:hgshen@outlook.com">
+              <EmailLogo />
+            </a>
+          </div>
+        </div>
+        
         <div className="home-link-wrapper">
           <Button
             bordered
